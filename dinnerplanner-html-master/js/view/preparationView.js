ee4eab00e69640b5ed3d0667f4cbbb9d5261a75c
ingridsorensen <iingridsorensen@gmail.com>
2015-03-02 15:56:58 +0100

    fixar innehåll på preparaten

--- dinnerplanner-html-master/js/view/preparationView.js
+++ dinnerplanner-html-master/js/view/preparationView.js
@@ -2,12 +2,14 @@
 var PreparationView = function (container, model) {
 
 	this.editButton = container.find("#edit");
+	this.numberOfGuests = container.find("#numberOfGuests");
 
 	model.addObserver(this);
 
-	
-	this.update = function(){
+	this.numberOfGuests.html(model.getNumberOfGuests());
 
+	this.update = function(){
+		this.numberOfGuests.html(model.getNumberOfGuests());
 	}
 }
  
